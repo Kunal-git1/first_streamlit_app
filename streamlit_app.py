076948e9d7fec367cead489323c6ba255d6bb442
Kunal Jamsutkar <114315202+Kunal-git1@users.noreply.github.com>
2022-10-03 00:56:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
-import pandas
+#import pandas
 #streamlit.stop()
 import snowflake.connector
-import requests
+#import requests
 from urllib.error import URLError
 
 streamlit.title("My Mom's New Healthy Diner")
