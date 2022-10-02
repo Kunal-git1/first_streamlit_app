1500f886a4810ee6a14a35e863e971e3905345ed
Kunal Jamsutkar <114315202+Kunal-git1@users.noreply.github.com>
2022-10-03 00:48:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,16 +42,20 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # write your own comment - what does this do?
 #streamlit.dataframe(fruityvice_normalized)
 
-
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response= request.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+   
+#New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice:
         streamlit.error("Please select a fruit to get information ")
    else:        
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
+        back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
 
 except URLError as e: 
     streamlit.error()
